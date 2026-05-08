a40b1c3bbe5889c167f17d2f78165ebbc8db812b
pe3hu <pe3hu3@gmail.com>
2026-05-08 09:52:40 +0500

    version 0.011
    1. added horde aproach
    2. added ore palette, horde border images

--- entities/horde/horde.gd
+++ entities/horde/horde.gd
@@ -2,24 +2,132 @@ extends TileMapLayer
 class_name Horde
 
 
-var ring_to_coords: Dictionary
+var ring_to_coord: Dictionary
+var coord_to_ring: Dictionary
+var coord_to_windrose: Dictionary
+var windrose_to_coord: Dictionary
+
+var spawn_coords: Array[Vector2i]
+var occupied_coords: Array[Vector2i]
+var detained_coords: Array[Vector2i]
+
 
 func _ready() -> void:
 	init_insects()
+	spawn_insects()
+	horde_approach()
 
 func init_insects() -> void:
-	for _y in range(-Catalog.HORDE_INSECT_SIZE.y, Catalog.HORDE_INSECT_SIZE.y + 1, 1):
-		for _x in range(-Catalog.HORDE_INSECT_SIZE.x, Catalog.HORDE_INSECT_SIZE.x + 1, 1):
+	for windrose in Catalog.windrose_to_direction.keys():
+		windrose_to_coord[windrose] = []
+	
+	for _y in range(-Catalog.HORDE_MAX_RING, Catalog.HORDE_MAX_RING + 1, 1):
+		for _x in range(-Catalog.HORDE_MAX_RING, Catalog.HORDE_MAX_RING + 1, 1):
 			var coord = Vector2i(_x, _y)
 			var ring = max(abs(_y), abs(_x))
 			
-			if !ring_to_coords.has(ring):
-				ring_to_coords[ring] = []
+			if !ring_to_coord.has(ring):
+				ring_to_coord[ring] = []
 			
-			ring_to_coords[ring].append(coord)
-			var coordinate = Vector2i(0, (ring % 4))
+			ring_to_coord[ring].append(coord)
+			coord_to_ring[coord] = ring
+			var windrose = null
 			
-			set_cell(coord, 0, coordinate)
+			if is_diagonal(coord):
+				if abs(_x) != 0:
+					var direction = coord / abs(_x)
+					windrose = Catalog.direction_to_windrose[direction]
+			else:
+				var l = min(abs(_x), abs(_y))
+				var x = sign(_x - sign(_x) * l)
+				var y = sign(_y - sign(_y) * l)
+				var direction = Vector2i(x, y)
+				windrose = Catalog.direction_to_windrose[direction]
+			
+			if windrose != null:
+				coord_to_windrose[coord] = windrose
+				windrose_to_coord[windrose].append(coord)
+	
+	for windrose in Catalog.orthogonal_windroses:
+		var max_ring_coords = ring_to_coord[Catalog.HORDE_MAX_RING]
+		max_ring_coords = max_ring_coords.filter(func (a): return windrose_to_coord[windrose].has(a))
+		#for coord in windrose_to_coord[windrose]:
+		#	var ring = coord_to_ring[coord]
+		#	if ring > 1:
+		#		set_cell(coord, 0, Catalog.windrose_to_palette[windrose])
+		var coord = max_ring_coords[Catalog.HORDE_MAX_RING - 1]
+		spawn_coords.append(coord)
 
-func add_axis() -> void:
-	pass
+func spawn_insects() -> void:
+	for _i in Catalog.STARTER_HORDE_INSECT_COUNT:
+		spawn_insect()
+
+func spawn_insect() -> void:
+	var coord = spawn_coords.pick_random()
+	var windrose = coord_to_windrose[coord]
+	set_cell(coord, 0, Catalog.windrose_to_palette[windrose])
+	update_spawn_coord(coord)
+
+func update_spawn_coord(coord_: Vector2i) -> void:
+	spawn_coords.erase(coord_)
+	occupied_coords.append(coord_)
+	var windrose = coord_to_windrose[coord_]
+	
+	for direction in Catalog.windrose_to_neighbour[windrose]:
+		var neighbour_coord = coord_ + direction
+		
+		if !is_diagonal(neighbour_coord):
+			if !occupied_coords.has(neighbour_coord):
+				spawn_coords.append(neighbour_coord)
+
+func horde_approach() -> void:
+	for _j in Catalog.HORDE_MAX_RING - 2:
+		for _i in range(occupied_coords.size()-1, -1, -1):
+			var coord = occupied_coords[_i]
+			insect_approach(coord)
+		
+		release_detained_coords()
+
+func insect_approach(coord_: Vector2i) -> void:
+	occupied_coords.erase(coord_)
+	var windrose = coord_to_windrose[coord_]
+	
+	set_cell(coord_, 0, Catalog.EMPTY_INSECT_COORD)
+	var windrose_approach = Catalog.windrose_to_mirror[windrose]
+	var direction = Catalog.windrose_to_direction[windrose_approach]
+	coord_ += direction
+	
+	if is_diagonal(coord_):
+		coord_ -= direction
+		detained_coords.append(coord_)
+	else:
+		if ring_to_coord[1].has(coord_):
+			coord_ -= direction
+		
+		occupied_coords.append(coord_)
+		set_cell(coord_, 0, Catalog.windrose_to_palette[windrose])
+
+func release_detained_coords() -> void:
+	if detained_coords.is_empty(): return
+	
+	while !detained_coords.is_empty():
+		var coord = detained_coords.pop_back()
+		var direction: Vector2i
+		
+		if abs(coord.x) > abs(coord.y):
+			direction = Vector2i(0, -sign(coord.y))
+		else:
+			direction = Vector2i(-sign(coord.x), 0)
+		
+		
+		coord += direction
+		if occupied_coords.has(coord):
+			coord -= direction
+		
+		var windrose = coord_to_windrose[coord]
+		occupied_coords.append(coord)
+		set_cell(coord, 0, Catalog.windrose_to_palette[windrose])
+
+
+func is_diagonal(coord_: Vector2i) -> bool:
+	return abs(coord_.x) == abs(coord_.y)
--- global/Catalog.gd
+++ global/Catalog.gd
@@ -14,7 +14,7 @@ const capsule_to_color = {
 #endregion
 
 #region windrose
-const windrose_to_vector = {
+const windrose_to_direction = {
 	Bozo.Windrose.N: Vector2i(0, 1),
 	Bozo.Windrose.NE: Vector2i(1, 1),
 	Bozo.Windrose.E: Vector2i(1, 0),
@@ -23,6 +23,17 @@ const windrose_to_vector = {
 	Bozo.Windrose.SW: Vector2i(-1, -1),
 	Bozo.Windrose.W: Vector2i(-1, 0),
 	Bozo.Windrose.NW: Vector2i(-1, 1),
+}
+
+const direction_to_windrose = {
+	Vector2i(0, 1): Bozo.Windrose.N,
+	Vector2i(1, 1): Bozo.Windrose.NE,
+	Vector2i(1, 0): Bozo.Windrose.E,
+	Vector2i(1, -1): Bozo.Windrose.SE,
+	Vector2i(0, -1): Bozo.Windrose.S,
+	Vector2i(-1, -1): Bozo.Windrose.SW,
+	Vector2i(-1, 0): Bozo.Windrose.W,
+	Vector2i(-1, 1): Bozo.Windrose.NW,
 }
 
 const windrose_to_mirror = {
@@ -35,7 +46,46 @@ const windrose_to_mirror = {
 	Bozo.Windrose.W: Bozo.Windrose.E,
 	Bozo.Windrose.NW: Bozo.Windrose.SE
 }
+
+const windrose_to_palette = {
+	Bozo.Windrose.N: Vector2i(0, 0),
+	Bozo.Windrose.NE: Vector2i(0, 1),
+	Bozo.Windrose.E: Vector2i(1, 0),
+	Bozo.Windrose.SE: Vector2i(1, 1),
+	Bozo.Windrose.S: Vector2i(2, 0),
+	Bozo.Windrose.SW: Vector2i(2, 1),
+	Bozo.Windrose.W: Vector2i(3, 0),
+	Bozo.Windrose.NW: Vector2i(3, 1)
+}
+
+const orthogonal_windroses = [
+	Bozo.Windrose.N,
+	Bozo.Windrose.E,
+	Bozo.Windrose.S,
+	Bozo.Windrose.W
+]
+
+const diagonal_windroses = [
+	Bozo.Windrose.NE,
+	Bozo.Windrose.SE,
+	Bozo.Windrose.SW,
+	Bozo.Windrose.NW
+]
+
+const windrose_to_neighbour = {
+	Bozo.Windrose.N: [Vector2i(-1, 0), Vector2i(1, 0)],
+	#Bozo.Windrose.NE: [Vector2i(1, 1)],
+	Bozo.Windrose.E: [Vector2i(0, -1), Vector2i(0, 1)],
+	#Bozo.Windrose.SE: [Vector2i(1, -1)],
+	Bozo.Windrose.S: [Vector2i(-1, 0), Vector2i(1, 0)],
+	#Bozo.Windrose.SW: [Vector2i(-1, -1)],
+	Bozo.Windrose.W: [Vector2i(0, -1), Vector2i(0, 1)],
+	#Bozo.Windrose.NW: [Vector2i(-1, 1)],
+}
 #endregion
 
 const INSECT_SPRITE_SIZE = Vector2(64, 64)
-const HORDE_INSECT_SIZE = Vector2i(5, 5)
+#const HORDE_INSECT_SIZE = Vector2i(6, 6)
+const HORDE_MAX_RING = 6
+const STARTER_HORDE_INSECT_COUNT = 13
+const EMPTY_INSECT_COORD = Vector2i(3, 3)
